77102eaf1dc0b8db1395f552f69dd191c1b8b937
pavans0951 <pavan20061974@gmail.com>
2025-09-12 19:20:00 +0530

    Update publicinfo.java

--- model/publicinfo.java
+++ model/publicinfo.java
@@ -1,6 +1,6 @@
 import java.util.*;
 package model;
-public class publicinfo {
+public class PublicInfo {
     private List<String> alerts;
     private List<String> safetyGuidelines;
     private List<String> shelters;
@@ -11,6 +11,7 @@ public class publicinfo {
         this.safetyGuidelines = new ArrayList<>();
         this.shelters = new ArrayList<>();
         this.emergencyContacts = new ArrayList<>();
+     }
         
          public void addAlert(String alert) {
         alerts.add(alert);
@@ -28,7 +29,38 @@ public class publicinfo {
         emergencyContacts.add(contact);
     }
 
-    public void addLiveUpdate(String update) {
-        liveUpdates.add(update);
+    public void displayPublicInfo() {
+        System.out.println("PUBLIC INFORMATION");
+
+        System.out.println("\nALERTS\n");
+            for(String a : alerts){
+                System.out.println("\n- " + a);
+            }
+              System.out.println("\nSAFETY GUIDELINES");
+        for (String g : safetyGuidelines) {
+            System.out.println("\n- " + g);
+        }
+
+        System.out.println("\nSHELTERS");
+        for (String s : shelters) {
+            System.out.println("\n- " + s);
+        }
+
+        System.out.println("\nEMERGENCY CONTACTS");
+        for (String c : emergencyContacts) {
+            System.out.println("\n- " + c);
+        }
     }
+    public static void main(String[] args) {
+        PublicInfo info = new PublicInfo();
+
+        // Adding sample data
+        info.addAlert("Flood warning in coastal area - Red Alert");
+        info.addSafetyGuideline("Do not step into floodwaters.");
+        info.addSafetyGuideline("Keep emergency kit ready.");
+        info.addShelterInfo("Shelter 1: Govt School, Capacity: 500");
+        info.addEmergencyContact("Disaster Helpline: 108");
+        info.addEmergencyContact("Police: 100");
+        info.addLiveUpdate("Road from City A to City B is closed due to landslide.");
+
 }
