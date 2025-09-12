f510df18c4ed99ff4e15691b63c240a5219c037e
pavans0951 <pavan20061974@gmail.com>
2025-09-12 19:11:56 +0530

    Update publicinfo.java

--- model/publicinfo.java
+++ model/publicinfo.java
@@ -1,4 +1,34 @@
+import java.util.*;
 package model;
 public class publicinfo {
+    private List<String> alerts;
+    private List<String> safetyGuidelines;
+    private List<String> shelters;
+    private List<String> emergencyContacts;
     
+     public PublicInfo() {
+        this.alerts = new ArrayList<>();
+        this.safetyGuidelines = new ArrayList<>();
+        this.shelters = new ArrayList<>();
+        this.emergencyContacts = new ArrayList<>();
+        
+         public void addAlert(String alert) {
+        alerts.add(alert);
+    }
+
+    public void addSafetyGuideline(String guideline) {
+        safetyGuidelines.add(guideline);
+    }
+
+    public void addShelterInfo(String shelter) {
+        shelters.add(shelter);
+    }
+
+    public void addEmergencyContact(String contact) {
+        emergencyContacts.add(contact);
+    }
+
+    public void addLiveUpdate(String update) {
+        liveUpdates.add(update);
+    }
 }
