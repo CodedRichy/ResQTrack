19c354f09ac94ee43484f4fb304c2409da947932
pavans0951 <pavan20061974@gmail.com>
2025-09-21 21:45:15 +0530

    Update hazard.java

--- service/hazard.java
+++ service/hazard.java
@@ -1,4 +1,30 @@
 package service;
 public class Hazard {
+    class Hazard {
+    private List<String> hazards;
+
+    public Hazard() {
+        hazards = new ArrayList<>();
+    }
+
+    public void addHazard(String hazard) {
+        hazards.add(hazard);
+        System.out.println("Hazard added: " + hazard);
+    }
+
+    public void viewHazard() {
+        System.out.println("\n===== LIST OF HAZARDS =====");
+        if (hazards.isEmpty()) {
+            System.out.println("No hazards recorded.");
+        } else {
+            for (int i = 0; i < hazards.size(); i++) {
+                System.out.println((i + 1) + ". " + hazards.get(i));
+            }
+        }
+        System.out.println("===========================\n");
+    }
+
+
     
+
 }
